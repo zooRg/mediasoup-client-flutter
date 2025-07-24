82acc583858d175ac0d9dafffcf81371fbda646b
Nikita Bystrov <zoorg71@gmail.com>
2025-07-24 11:39:27 +0300

    main: fix

--- example/lib/features/producers/bloc/producers_bloc.dart
+++ example/lib/features/producers/bloc/producers_bloc.dart
@@ -8,12 +8,10 @@ part 'producers_event.dart';
 part 'producers_state.dart';
 
 class ProducersBloc extends Bloc<ProducersEvent, ProducersState> {
-  ProducersBloc() : super(ProducersState());
+  ProducersBloc() : super(const ProducersState());
 
   @override
-  Stream<ProducersState> mapEventToState(
-    ProducersEvent event,
-  ) async* {
+  Stream<ProducersState> mapEventToState(ProducersEvent event) async* {
     if (event is ProducerAdd) {
       yield* _mapProducerAddToState(event);
     } else if (event is ProducerRemove) {
@@ -27,76 +25,98 @@ class ProducersBloc extends Bloc<ProducersEvent, ProducersState> {
 
   Stream<ProducersState> _mapProducerAddToState(ProducerAdd event) async* {
     switch (event.producer.source) {
-      case 'mic': {
-        yield ProducersState.copy(state, mic: event.producer);
+      case 'mic':
+        {
+          yield ProducersState.copy(state, mic: event.producer);
+          break;
+        }
+      case 'webcam':
+        {
+          yield ProducersState.copy(state, webcam: event.producer);
+          break;
+        }
+      case 'screen':
+        {
+          yield ProducersState.copy(state, screen: event.producer);
+          break;
+        }
+      default:
         break;
-      }
-      case 'webcam': {
-        yield ProducersState.copy(state, webcam: event.producer);
-        break;
-      }
-      case 'screen': {
-        yield ProducersState.copy(state, screen: event.producer);
-        break;
-      }
-      default: break;
     }
   }
 
-  Stream<ProducersState> _mapProducerRemoveToState(ProducerRemove event) async* {
+  Stream<ProducersState> _mapProducerRemoveToState(
+    ProducerRemove event,
+  ) async* {
     switch (event.source) {
-      case 'mic': {
-        state.mic?.close();
-        yield ProducersState.removeMic(state);
+      case 'mic':
+        {
+          state.mic?.close();
+          yield ProducersState.removeMic(state);
+          break;
+        }
+      case 'webcam':
+        {
+          state.webcam?.close();
+          yield ProducersState.removeWebcam(state);
+          break;
+        }
+      case 'screen':
+        {
+          state.screen?.close();
+          yield ProducersState.removeScreen(state);
+          break;
+        }
+      default:
         break;
-      }
-      case 'webcam': {
-        state.webcam?.close();
-        yield ProducersState.removeWebcam(state);
-        break;
-      }
-      case 'screen': {
-        state.screen?.close();
-        yield ProducersState.removeScreen(state);
-        break;
-      }
-      default: break;
     }
   }
 
-  Stream<ProducersState> _mapProducerResumeToState(ProducerResumed event) async* {
+  Stream<ProducersState> _mapProducerResumeToState(
+    ProducerResumed event,
+  ) async* {
     switch (event.source) {
-      case 'mic': {
-        yield ProducersState.copy(state, mic: state.mic!.resumeCopy());
+      case 'mic':
+        {
+          yield ProducersState.copy(state, mic: state.mic!.resumeCopy());
+          break;
+        }
+      case 'webcam':
+        {
+          yield ProducersState.copy(state, webcam: state.webcam!.resumeCopy());
+          break;
+        }
+      case 'screen':
+        {
+          yield ProducersState.copy(state, screen: state.screen?.resumeCopy());
+          break;
+        }
+      default:
         break;
-      }
-      case 'webcam': {
-        yield ProducersState.copy(state, webcam: state.webcam!.resumeCopy());
-        break;
-      }
-      case 'screen': {
-        yield ProducersState.copy(state, screen: state.screen?.resumeCopy());
-        break;
-      }
-      default: break;
     }
   }
 
-  Stream<ProducersState> _mapProducerPausedToState(ProducerPaused event) async* {
+  Stream<ProducersState> _mapProducerPausedToState(
+    ProducerPaused event,
+  ) async* {
     switch (event.source) {
-      case 'mic': {
-        yield ProducersState.copy(state, mic: state.mic!.pauseCopy());
+      case 'mic':
+        {
+          yield ProducersState.copy(state, mic: state.mic!.pauseCopy());
+          break;
+        }
+      case 'webcam':
+        {
+          yield ProducersState.copy(state, webcam: state.webcam!.pauseCopy());
+          break;
+        }
+      case 'screen':
+        {
+          yield ProducersState.copy(state, screen: state.screen!.pauseCopy());
+          break;
+        }
+      default:
         break;
-      }
-      case 'webcam': {
-        yield ProducersState.copy(state, webcam: state.webcam!.pauseCopy());
-        break;
-      }
-      case 'screen': {
-        yield ProducersState.copy(state, screen: state.screen!.pauseCopy());
-        break;
-      }
-      default: break;
     }
   }
 }
