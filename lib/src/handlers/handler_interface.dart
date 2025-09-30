9049b347dd5f537c166de49593e6329c5761758f
Nikita Bystrov <zoorg71@gmail.com>
2025-09-30 17:22:25 +0300

    main: bump

--- lib/src/handlers/handler_interface.dart
+++ lib/src/handlers/handler_interface.dart
@@ -90,7 +90,8 @@ class RTCIceServer {
 
   RTCIceServer.fromMap(Map<String, dynamic> data)
       : credential = data['credential'],
-        credentialType = RTCIceCredentialTypeToString.fromString(data['credentialType']),
+        credentialType = RTCIceCredentialTypeToString.fromString(
+            data['credentialType'] ?? 'password'),
         urls = data['urls'],
         username = data['username'];
 }
@@ -258,9 +259,11 @@ abstract class HandlerInterface extends EnhancedEventEmitter {
   Future<void> stopSending(String localId);
   Future<void> replaceTrack(ReplaceTrackOptions options);
   Future<void> setMaxSpatialLayer(SetMaxSpatialLayerOptions options);
-  Future<void> setRtpEncodingParameters(SetRtpEncodingParametersOptions options);
+  Future<void> setRtpEncodingParameters(
+      SetRtpEncodingParametersOptions options);
   Future<List<StatsReport>> getSenderStats(String localId);
-  Future<HandlerSendDataChannelResult> sendDataChannel(SendDataChannelArguments options);
+  Future<HandlerSendDataChannelResult> sendDataChannel(
+      SendDataChannelArguments options);
   Future<HandlerReceiveResult> receive(HandlerReceiveOptions options);
   Future<void> stopReceiving(
     String localId,
